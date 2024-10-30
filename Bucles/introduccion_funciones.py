95005db316e40ee8ffe2ef14bb88bd387f8f4f6a
HDanny7 <german.colon@udea.edu.co>
2024-10-29 20:55:50 -0500

    nuevo

---
+++ Bucles/introduccion_funciones.py
@@ -0,0 +1,15 @@
+# Introduccion a las funciones.
+# Son bloques de codigo que podemos llmar cuando los necesitemos de manera practica.
+
+# palabra reservada DEF
+
+''' def nombre_de_la_funcion(parametro1, parametro2, etc):
+    # codigo de la funcion
+    return # solo si hay q devolver valores'''
+
+# Ejemplo
+def saludar():
+    nombre = input('Introduzca su nombre, por favor: ')
+    print(f'Muy buenas, {nombre}!')
+saludar()
+
---
+++ Bucles/while_else.py
@@ -0,0 +1,17 @@
+# Bucles con ELSE
+# Con bucle FOR
+'''for i in range(5):
+    print(f'El valor del bucle es {i}.')
+    
+else:
+        print('Frase del bloque else.')
+'''
+numero = int(input('Introduzca un numero: '))
+
+while numero < 5:
+    print(f'El numero ingresado es: {numero}.')
+    numero += 1
+else:
+    print('Codigo desde el bloque else.')
+
+
