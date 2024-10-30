14bb98faf18db5f725bc9725abea0a92c677d947
HDanny7 <german.colon@udea.edu.co>
2024-10-29 22:15:18 -0500

    nuevo

---
+++ Bucles/funciones_predefinidas.py
@@ -0,0 +1,5 @@
+# Vienen integradas con las funciones de python
+'''STR, INPUT, PRINT, entre otras entran en esta categoria,
+l idea es teenr una idea aproximada de su funcion y adaptarla a 
+las necesidades de lo que quiere, toca programas mucho para ir cogiendo practica 
+en todo lo aprendido.'''
