e8daf7c2cce6e615cb7ea0b7b15cde5d5b5f32bb
HDanny7 <german.colon@udea.edu.co>
2024-12-06 23:28:21 -0500

    New

---
+++ Entornos virtuales/expresiones_arbitrarias_clave.py
@@ -0,0 +1,10 @@
+# KWARGS: Permite pasar un diccionario de argumentos en ves de una tupla de valores.
+def diccionario(**kwargs):
+    print(kwargs)
+diccionario(nombre='Gabriela',
+            apellidos='Gómez de la barca',
+            edad= '27')
+# ARGS y KWARGS pueden ir juntos en una funcion.
+
+
+
---
+++ Entornos virtuales/expresiones_argumentos.py
@@ -0,0 +1,12 @@
+# Se pueden poner expresiones de cualquier tipo como argumentos, se pasa primero y sus resultados seran presentados como argumentos.
+'''def suma(a):
+    print(f'El resultado es: {a}.')
+suma(10+20)'''
+def resolver_expresion(a):
+    print(f'El resultado es: {a}.')
+resolver_expresion(a = 10 == 10)
+
+
+
+
+
